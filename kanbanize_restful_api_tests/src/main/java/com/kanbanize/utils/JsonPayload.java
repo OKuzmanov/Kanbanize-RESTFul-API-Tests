b521f0633e02d3670023f154e894d8fa17800520
OKuzmanov <okuzmanov@outlook.com>
2023-03-08 11:24:49 +0200

    Update JsonPayload class

--- kanbanize_restful_api_tests/src/main/java/com/kanbanize/utils/JsonPayload.java
+++ kanbanize_restful_api_tests/src/main/java/com/kanbanize/utils/JsonPayload.java
@@ -3,61 +3,61 @@ package com.kanbanize.utils;
 public class JsonPayload {
 
     public static String createCard(String title, int colId, int laneId, int position, int ownerId, int size, int priority, String color, String description, String deadline) {
-        return "{\n" +
-                "    \"title\": \"" + title + "\",\n" +
-                "    \"column_id\": " + colId + ",\n" +
-                "    \"lane_id\": " + laneId + ",\n" +
-                "    \"position\": " + position + ",\n" +
-                "    \"owner_user_id\": " + ownerId + ",\n" +
-                "    \"size\": " + size + ",\n" +
-                "    \"priority\": " + priority + ",\n" +
-                "    \"color\": \"" + color + "\",\n" +
-                "    \"description\": \"" + description + "\",\n" +
-                "    \"deadline\": \"" + deadline + "\"\n" +
+        return "{" +
+                "    \"title\": \"" + title + "\"," +
+                "    \"column_id\": " + colId + "," +
+                "    \"lane_id\": " + laneId + "," +
+                "    \"position\": " + position + "," +
+                "    \"owner_user_id\": " + ownerId + "," +
+                "    \"size\": " + size + "," +
+                "    \"priority\": " + priority + "," +
+                "    \"color\": \"" + color + "\"," +
+                "    \"description\": \"" + description + "\"," +
+                "    \"deadline\": \"" + deadline + "\"" +
                 "}";
     }
 
     public static String updateCardColumn(int newColumnId) {
-        return "{\n" +
-                "    \"column_id\": " + newColumnId + "\n" +
+        return "{" +
+                "    \"column_id\": " + newColumnId +
                 "}";
     }
 
     public static String addSubtask(String description, int ownerId) {
-        return "{\n" +
-                "  \"description\": \"" + description + "\",\n" +
-                "  \"owner_user_id\": " + ownerId + "\n" +
+        return "{" +
+                "  \"description\": \"" + description + "\"," +
+                "  \"owner_user_id\": " + ownerId +
                 "}";
     }
 
     public static String updateCardConvertSubtaskToChildCard(int subtaskId, int cardId, String title, String description, int ownerId,
                                                              int priority, String color, int position, int columnId, int laneId) {
-        return "{\n" +
-                "    \"subtasks_to_convert_into_cards\": [\n" +
-                "        {\n" +
-                "            \"subtask_id\": \"" + subtaskId + "\",\n" +
-                "            \"title\": \"" + title + "\",\n" +
-                "            \"description\": \"" + description + "\",\n" +
-                "            \"owner_user_id\": " + ownerId + ",\n" +
-                "            \"priority\": " + priority + ",\n" +
-                "            \"color\": \"" + color + "\",\n" +
-                "            \"position\": " + position + ",\n" +
-                "            \"column_id\": " + columnId + ",\n" +
-                "            \"lane_id\": " + laneId + ",\n" +
-                "            \"links_to_existing_cards_to_add_or_update\": [\n" +
-                "                {\n" +
-                "                    \"linked_card_id\": \"" + cardId + "\",\n" +
-                "                    \"link_type\": \"parent\"\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        }\n" +
-                "    ]\n" +
+        return "{" +
+                "    \"subtasks_to_convert_into_cards\": [" +
+                "        {" +
+                "            \"subtask_id\": \"" + subtaskId + "\"," +
+                "            \"title\": \"" + title + "\"," +
+                "            \"description\": \"" + description + "\"," +
+                "            \"owner_user_id\": " + ownerId + "," +
+                "            \"priority\": " + priority + "," +
+                "            \"color\": \"" + color + "\"," +
+                "            \"position\": " + position + "," +
+                "            \"column_id\": " + columnId + "," +
+                "            \"lane_id\": " + laneId + "," +
+                "            \"links_to_existing_cards_to_add_or_update\": [" +
+                "                {" +
+                "                    \"linked_card_id\": \"" + cardId + "\"," +
+                "                    \"link_type\": \"parent\"" +
+                "                }" +
+                "            ]" +
+                "        }" +
+                "    ]" +
                 "}";
     }
 
     public static String updateCardSize(int newSize) {
-        return "{\n" +
-                "    \"size\": " + newSize + "\n" +
+        return "{" +
+                "    \"size\": " + newSize +
                 "}";
     }
 }
